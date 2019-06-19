d6f7bb027d39b326867116f85bf65c2f43b44378
A-hu <louis0911317399@gmail.com>
2019-06-20 01:28:10 +0800

    MongoDB connection settings

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,9 @@
+const credentials = require('./credentials');
+
 module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: credentials.mongoURI,
   cookieKey: '123123123'
 };
